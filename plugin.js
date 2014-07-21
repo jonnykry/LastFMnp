f27c4e7d05de8fbab78ca33f3dcbb40c7be2e2e7
jonnykry <jonnykry93@gmail.com>
2014-07-20 20:50:20 -0500

    Added comments.

--- plugin.js
+++ plugin.js
@@ -1,4 +1,5 @@
 // To use this widget, edit your username / api key into the URL
+// Please avoid using my key :')
 var xml = httpGet("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jonnydoesmusic&api_key=c64600ddca04dfc310703c59fe1b5230");
 
 var track = xml.getElementsByTagName("track");
@@ -36,6 +37,9 @@ function displayTrack(track)
     //albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);
     //var imgobject = $("<img />").attr("src", albumimg);
     
+
+    // This text contains the html that is appended to that page.
+    // Edit the link to contain your username. 
     txt = "<div id='text' style='padding-top: 5px; color: #FFF;'><marquee behavior='scroll' scrollamount='5' direction='left' width='425'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank' style='color: white; font-family: Arial;'>Currently listening to: " 
     + artist + ' - "' + song + '"' + " &mdash; Click to view my LastFM" + "</a></marquee></div>";
     return txt;
