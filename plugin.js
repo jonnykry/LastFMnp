8af478c28864c587d4757465cbc7a2442555e7c7
jonnykry <jonnykry93@gmail.com>
2014-07-21 23:01:11 -0500

    Fixed bug where lack of album covers in XML broke the widget.

--- plugin.js
+++ plugin.js
@@ -5,11 +5,6 @@ var xml = httpGet("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks
 var track = xml.getElementsByTagName("track");
 var i = 0;
 
-// If you want album cover:
-
-src = track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue;
-document.write();
-
 var txt = displayTrack(track);
 
 document.write(txt);
@@ -27,13 +22,31 @@ function httpGet(theUrl)
 
 function displayTrack(track)
 {
-    artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
-    song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-    streamable = (track[0].getElementsByTagName("streamable")[0].childNodes[0].nodeValue);
-    album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
-    
-    // more album cover things
-    albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);
+    var artist = null;
+    var song = null;
+    var streamable = null;
+    var album = null;
+    var albumimg = null;
+    var txt = "";
+
+    /* 
+    Sometimes LastFM does not supply the image. This will account for it.
+    This occurs whenever there is a (feat. ____) tag on the song.
+    Still working on finding the album for that song.
+     */
+    try {
+        artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
+        song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+        streamable = (track[0].getElementsByTagName("streamable")[0].childNodes[0].nodeValue);
+        album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
+        albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);
+    } catch(err) {
+        if(albumimg == null) {    
+            txt = "<div id='text'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank'>" 
+            + artist + '</strong> - "' + song + '"' + "</a></div>";
+            return txt;            
+        }
+    }
 
     // This text contains the html that is appended to that page.
     // Edit the link to contain your username. 
