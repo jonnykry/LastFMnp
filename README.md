556f6be3fa8a9513387677e3910c97c318eedffd
jonnykry <jonnykry93@gmail.com>
2015-09-20 19:44:22 -0500

    Code update, trying to make this consumable

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A widget for LastFM that shows the most current track you listened to - a great 
 
 Feel free to fork/clone and modify your own version!
 
-View the demo [here](http://www.jonnykrysh.com/lastfmnp).
+View the demo [here](http://www.jonnykrysh.me/lastfmnp).
 
 Created using the [LastFM API](http://www.last.fm/api).
--- plugin.js
+++ plugin.js
@@ -1,62 +1,75 @@
-// To use this widget, edit your username / api key into the URL
-// Please avoid using my key :')
-var artist = null;
-var song = null;
-var streamable = null;
-var album = null;
-var albumimg = null;
-var txt = "";
 
-var xml = httpGet("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=jonnydoesmusic&api_key=c64600ddca04dfc310703c59fe1b5230");
+// To re-use this component, you must enter your own strings below
+var LASTFM_USERNAME = 'jonnydoesmusic';
+var LASTFM_URL = 'http://www.last.fm/user/jonnydoesmusic';
 
-var track = xml.getElementsByTagName("track");
-var i = 0;
+// For the API key, add a key to keys.json in the root directory with the same format as below
+//
+// {
+//    "apikeys": {
+//        "lastfm": YOUR_KEY_HERE
+//    }
+// }
+//
 
-var txt = displayTrack(track);
 
-document.write(txt);
+$(document).ready( function() {
+    var artist = null;
+    var song = null;
+    var album = null;
+    var albumimg = null;
+    var widgetSelector = $('#widget');
 
-$("#widget").append(text);
+    $.ajax({
+        dataType: 'json',
+        url: 'keys.json',
+        success: function(response) {
+            var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + LASTFM_USERNAME + '&api_key=' + response.apikeys.lastfm;
 
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseXML;
-}
-
-function displayTrack(track)
-{
-    // Sometimes LastFM does not supply the image. This will account for it.
-    // This occurs whenever there is a (feat. ____) tag on the current song.
-    // Taking the song before it works.
-    try {
-        artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
-        song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-
-        // Take prev. song instead
-        if(song.indexOf('(') > -1) {
-            artist = (track[1].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
-            song = (track[1].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-            album = (track[1].getElementsByTagName("album")[0].childNodes[0].nodeValue);
-            albumimg = (track[1].getElementsByTagName("image")[0].childNodes[0].nodeValue);                 
+            // Request data from LastFM
+            $.ajax({
+                url: url,
+                success: function(response) {
+                    displayTrack(response.getElementsByTagName('track'));
+                }
+            });
         }
-        else {
-            // add in album and img from current song
-            album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
-            albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);   
+    });
+
+    function displayTrack(track)
+    {
+
+        // Sometimes LastFM does not supply the image, which throws an error
+        // We account for this with the try/catch.
+        try {
+            song = (track[0].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+
+            // Take previous song if current one hasn't loaded
+            if(song.indexOf('(') > -1) {
+                artist = (track[1].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
+                song = (track[1].getElementsByTagName("name")[0].childNodes[0].nodeValue);
+                album = (track[1].getElementsByTagName("album")[0].childNodes[0].nodeValue);
+                albumimg = (track[1].getElementsByTagName("image")[0].childNodes[0].nodeValue);
+            }
+            else {
+                // add in album and img from current song
+                artist = (track[0].getElementsByTagName("artist")[0].childNodes[0].nodeValue);
+                album = (track[0].getElementsByTagName("album")[0].childNodes[0].nodeValue);
+                albumimg = (track[0].getElementsByTagName("image")[0].childNodes[0].nodeValue);
+            }
+        } catch(err) {
+            text = "<a href='" + LASTFM_URL + "' target='_blank'>"
+                + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + ' - "' + song + '"' + "</strong></a>";
+            widgetSelector.append(text);
+            return text;
         }
-    } catch(err) {
-        txt = "<div id='text'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank'>" 
-        + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + '</strong> - "' + song + '"' + "</a></div>";
-        return txt;
+
+        // This text contains the html that is appended to that page.
+        // Edit the link to contain your username.
+        text = "<a href='" + LASTFM_URL + "' target='_blank'>"
+            + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + ' - "' + song + '"' + "</strong></a>";
+        widgetSelector.append(text);
+        return text;
     }
 
-    // This text contains the html that is appended to that page.
-    // Edit the link to contain your username. 
-    txt = "<div id='text'><a href='http://www.last.fm/user/jonnydoesmusic' target='_blank'>" 
-    + "<img src='" + albumimg + "' style='padding-right: 5px;'/> #np - <strong>" + artist + '</strong> - "' + song + '"' + "</a></div>";
-    return txt;
-}
+});
